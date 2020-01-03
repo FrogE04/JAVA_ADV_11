55cc42a7fad309c4cd5450179df3b18dfb012e8a
FrogE04 <fred870126@gmail.com>
2020-01-03 14:56:49 +0800

    Initial commit

---
+++ HW11/src/main/java/com/example/hw11/MyClass.java
@@ -0,0 +1,8 @@
+package com.example.hw11;
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String[] args) {
+        Num prizeNumber = new Num("7_8.txt");
+        prizeNumber.Judge();
+    }
+}
